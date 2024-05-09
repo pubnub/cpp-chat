ccd242319dd89bbe169b0d3b659995d95f2b65c7
Xavrax <matedahlke@gmail.com>
2024-05-09 09:42:33 +0200

    add declspec

--- include/lib.hpp
+++ include/lib.hpp
@@ -1,10 +1,17 @@
 #include <string>
 
+#if defined(_WIN32) || defined(__WIN32__) || defined(__WINDOWS__) || defined(__MINGW32__)
+#define DLL_EXPORT __declspec(dllexport)
+#else
+#define DLL_EXPORT
+#endif
+
+
 class Library {
     public:
-        __declspec(dllexport) int publish(std::string channel, std::string message);
+        DLL_EXPORT int publish(std::string channel, std::string message);
 };
 
-__declspec(dllexport) int publish_fn(std::string channel, std::string message);
+DLL_EXPORT int publish_fn(std::string channel, std::string message);
 
-__declspec(dllexport) int publish_simple_fn();
+DLL_EXPORT int publish_simple_fn();
