39a02d4b1d248006eef2ea932cfa947271a08b8a
KamilGronek <kamgronek@wp.pl>
2024-05-13 10:53:27 +0200

    Work on Chat Object. Add Pubnub namespace.

--- include/chat.h
+++ include/chat.h
@@ -8,19 +8,25 @@ extern "C" {
     #include "core/pubnub_api_types.h"
 }
 
-class Chat
+namespace Pubnub
 {
-    public:
+    PN_CHAT_EXPORT class Chat
+    {
+        public:
 
-    PN_CHAT_EXPORT void init();
-    PN_CHAT_EXPORT void publish_message(std::string channel, std::string message);
-    PN_CHAT_EXPORT void deinit();
+        Chat(){};
+        ~Chat(){};
 
-    private:
-    pubnub_t *ctx_pub;
-    const char* publish_key = "pub-c-79961364-c3e6-4e48-8d8d-fe4f34e228bf";
-    const char* subscribe_key = "sub-c-2b4db8f2-c025-4a76-9e23-326123298667";
-    std::string user_id = "TestUser";
-};
+        PN_CHAT_EXPORT void init();
+        PN_CHAT_EXPORT void publish_message(std::string channel, std::string message);
+        PN_CHAT_EXPORT void publish_message(const char* channel, const char* message);
+        PN_CHAT_EXPORT void deinit();
 
+        private:
+        pubnub_t *ctx_pub;
+        const char* publish_key = "pub-c-79961364-c3e6-4e48-8d8d-fe4f34e228bf";
+        const char* subscribe_key = "sub-c-2b4db8f2-c025-4a76-9e23-326123298667";
+        const char* user_id = "TestUser";
+    };
+}
 #endif /* CHAT_H */
--- src/chat.cpp
+++ src/chat.cpp
@@ -7,22 +7,31 @@ extern "C" {
 #include "core/pubnub_ntf_sync.h"
 }
 
+using namespace Pubnub;
+
 void Chat::init()
 {
     std::cout << "Init Chat SDK\n";
     
     ctx_pub = pubnub_alloc();
     pubnub_init(ctx_pub, publish_key, subscribe_key);
-    pubnub_set_user_id(ctx_pub, user_id.c_str());
+    pubnub_set_user_id(ctx_pub, user_id);
 
     std::cout << "Chat SDK Initialized\n";
 }
 
 void Chat::publish_message(std::string channel, std::string message)
 {
+    publish_message(channel.c_str(), message.c_str());
+}
+
+void Chat::publish_message(const char* channel, const char*message)
+{
     std::cout << "Publish message\n";
 
-    pubnub_publish(ctx_pub, channel.c_str(), message.c_str());
+    pubnub_publish(ctx_pub, channel, message);
+
+    pubnub_await(ctx_pub);
 
     std::cout << "Message published\n";
 }
