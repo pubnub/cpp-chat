0284e0e1d35d48931bf45e62efd6cd8f7f70d3cb
Xavrax <matedahlke@gmail.com>
2024-05-28 09:26:52 +0200

    some c exported functions

--- include/c_functions/c_chat.hpp
+++ include/c_functions/c_chat.hpp
@@ -1,6 +1,7 @@
 #include "chat.hpp"
 #include "export.hpp"
 #include "extern.hpp"
+#include "chat/channel.hpp"
 
 PN_CHAT_EXTERN PN_CHAT_EXPORT Pubnub::Chat* pn_chat_new(
         const char* publish,
@@ -12,4 +13,32 @@ PN_CHAT_EXTERN PN_CHAT_EXPORT void pn_chat_delete(Pubnub::Chat* chat);
 PN_CHAT_EXTERN PN_CHAT_EXPORT void pn_chat_send_message(
         Pubnub::Chat* chat,
         const char* message); 
+
+PN_CHAT_EXTERN PN_CHAT_EXPORT Pubnub::Channel* pn_chat_create_public_conversation(
+        Pubnub::Chat* chat,
+        const char* channel_id,
+        Pubnub::ChatChannelDataChar channel_data);
+
+PN_CHAT_EXTERN PN_CHAT_EXPORT Pubnub::Channel* pn_chat_update_channel(
+        Pubnub::Chat* chat,
+        std::string channel_id,
+        Pubnub::ChatChannelDataChar channel_data);
+
+PN_CHAT_EXTERN PN_CHAT_EXPORT Pubnub::Channel* pn_chat_get_channel(
+        Pubnub::Chat* chat,
+        const char* channel_id);
+
+// TODO: get channels...
+
+PN_CHAT_EXTERN PN_CHAT_EXPORT void pn_chat_delete_channel(
+        Pubnub::Chat* chat,
+        const char* channel_id);
+
+PN_CHAT_EXTERN PN_CHAT_EXPORT void pn_chat_set_restrictions(
+        Pubnub::Chat* chat,
+        const char* user_id,
+        const char* channel_id,
+        bool ban_user, 
+        bool mute_user,
+        const char* reason);
 
--- src/c_functions/c_chat.cpp
+++ src/c_functions/c_chat.cpp
@@ -1,4 +1,5 @@
 #include "c_functions/c_chat.hpp"
+#include "chat/channel.hpp"
 
 Pubnub::Chat* pn_chat_new(
         const char* publish,
@@ -15,12 +16,24 @@ void pn_chat_delete(Pubnub::Chat* chat) {
     delete chat;
 }
 
-void pn_chat_send_message(
+Pubnub::Channel* pn_chat_create_public_conversation(
         Pubnub::Chat* chat,
-        const char* message) {
-    throw std::runtime_error("Not implemented");
+        const char* channel_id,
+        Pubnub::ChatChannelDataChar channel_data) {
+    return chat->create_public_conversation((const char*)channel_id, channel_data);
 }
 
+Pubnub::Channel* pn_chat_update_channel(
+        Pubnub::Chat* chat,
+        const char* channel_id,
+        Pubnub::ChatChannelDataChar channel_data) {
+    return chat->update_channel(channel_id, channel_data);
+}
 
+Pubnub::Channel* pn_chat_get_channel(
+        Pubnub::Chat* chat,
+        const char* channel_id) {
+    return chat->get_channel(channel_id);
+}
 
 
