ec0d7966078ee9da757fb580dd7ea0f510c3ca5c
Xavrax <matedahlke@gmail.com>
2024-06-06 14:51:16 +0200

    oops I did it again

--- example/main.cpp
+++ example/main.cpp
@@ -3,8 +3,8 @@
 #include <iostream>
 
 int main() {
-    std::string pub_key = "pub-c-2451c2cf-9f04-446b-8f85-e06564095e55";
-    std::string sub_key = "sub-c-d16ff59f-b415-4ef9-8c29-ddda64fa2b43";
+    std::string pub_key = "demo";
+    std::string sub_key = "demo";
     std::string user = "hehehe";
 
     Pubnub::Chat chat(pub_key, sub_key, user);
