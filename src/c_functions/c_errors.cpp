2fb6eaa23e87903ac44798feda0e403f46d7ed02
PUBNUB\jakub.grzesiowski <jakub.grzesiowski@pubnub.com>
2024-06-11 14:28:24 +0200

    add missing header include to c_errors.cpp

--- src/c_functions/c_errors.cpp
+++ src/c_functions/c_errors.cpp
@@ -1,4 +1,5 @@
 #include <cstring>
+#include "c_functions/c_errors.hpp"
 
 static char* pn_c_error_message_ptr = nullptr;
 
@@ -8,6 +9,7 @@ void pn_c_set_error_ptr(char* ptr) {
 
 void pn_c_set_error_message(const char* message) {
     if (pn_c_error_message_ptr != nullptr) {
+        //memcpy(pn_c_error_message_ptr, message, strlen(message) + 1);
         strcpy(pn_c_error_message_ptr, message);
     }
 }
