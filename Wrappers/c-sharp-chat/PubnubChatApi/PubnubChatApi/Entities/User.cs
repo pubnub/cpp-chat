0a3b831f2c5a4555a66dee92172a6a68c09e3214
Xavrax <matedahlke@gmail.com>
2024-06-17 13:17:12 +0200

    Some user docs

--- Wrappers/c-sharp-chat/PubnubChatApi/PubnubChatApi/Entities/User.cs
+++ Wrappers/c-sharp-chat/PubnubChatApi/PubnubChatApi/Entities/User.cs
@@ -7,6 +7,12 @@ using PubnubChatApi.Utilities;
 
 namespace PubNubChatAPI.Entities
 {
+    /// <summary>
+    /// Represents a user in the chat. 
+    /// <para>
+    /// You can get information about the user, update the user's data, delete the user, set restrictions on the user,
+    /// </para>
+    /// </summary>
     public class User : PointerWrapper
     {
         #region DLL Imports
@@ -59,6 +65,12 @@ namespace PubNubChatAPI.Entities
 
         #endregion
 
+        /// <summary>
+        /// The user's user name. 
+        /// <para>
+        /// This might be user's display name in the chat.
+        /// </para>
+        /// </summary>
         public string UserName
         {
             get
@@ -69,6 +81,12 @@ namespace PubNubChatAPI.Entities
             }
         }
 
+        /// <summary>
+        /// The user's external id.
+        /// <para>
+        /// This might be user's id in the external system (e.g. Database, CRM, etc.)
+        /// </para>
+        /// </summary>
         public string ExternalId
         {
             get
@@ -79,6 +97,12 @@ namespace PubNubChatAPI.Entities
             }
         }
 
+        /// <summary>
+        /// The user's profile url.
+        /// <para>
+        /// This might be user's profile url to download the profile picture.
+        /// </para>
+        /// </summary>
         public string ProfileUrl
         {
             get
@@ -89,6 +113,12 @@ namespace PubNubChatAPI.Entities
             }
         }
 
+        /// <summary>
+        /// The user's email.
+        /// <para>
+        /// This should be user's email address.
+        /// </para>
+        /// </summary>
         public string Email
         {
             get
@@ -99,6 +129,12 @@ namespace PubNubChatAPI.Entities
             }
         }
 
+        /// <summary>
+        /// The user's custom data.
+        /// <para>
+        /// This might be any custom data that you want to store for the user.
+        /// </para>
+        /// </summary>
         public string CustomData
         {
             get
@@ -109,6 +145,12 @@ namespace PubNubChatAPI.Entities
             }
         }
 
+        /// <summary>
+        /// The user's status.
+        /// <para>
+        /// This is a string that represents the user's status.
+        /// </para>
+        /// </summary>
         public string Status
         {
             get
@@ -119,6 +161,12 @@ namespace PubNubChatAPI.Entities
             }
         }
 
+        /// <summary>
+        /// The user's data type.
+        /// <para>
+        /// This is a string that represents the user's data type.
+        /// </para>
+        /// </summary>
         public string DataType
         {
             get
@@ -130,6 +178,25 @@ namespace PubNubChatAPI.Entities
         }
 
         private Chat chat;
+
+        /// <summary>
+        /// Event that is triggered when the user is updated.
+        /// <para>
+        /// This event is triggered when the user's data is updated.
+        /// You can subscribe to this event to get notified when the user is updated.
+        /// </para>
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// // var user = // ...;
+        /// user.OnUserUpdated += (user) =>
+        /// {
+        ///    Console.WriteLine($"User {user.UserName} is updated.");
+        /// };
+        /// </code>
+        /// </example>
+        /// <seealso cref="UpdateUser"/>
+        /// <seealso cref="User"/>
         public event Action<User> OnUserUpdated;
 
         internal User(Chat chat, string userId, IntPtr userPointer) : base(userPointer, userId)
@@ -149,22 +216,78 @@ namespace PubNubChatAPI.Entities
             OnUserUpdated?.Invoke(this);
         }
 
+        /// <summary>
+        /// Updates the user.
+        /// <para>
+        /// This method updates the user's data.
+        /// </para>
+        /// </summary>
+        /// <param name="updatedData">The updated data for the user.</param>
+        /// <exception cref="PubNubCCoreException">
+        /// This exception might be thrown when any error occurs while updating the user.
+        /// </exception>
+        /// <example>
+        /// <code>
+        /// var user = // ...;
+        /// user.UpdateUser(new ChatUserData
+        /// {
+        ///    UserName = "New User Name",
+        /// });
+        /// </code>
+        /// </example>
+        /// <seealso cref="ChatUserData"/>
         public void UpdateUser(ChatUserData updatedData)
         {
             chat.UpdateUser(Id, updatedData);
         }
 
+        /// <summary>
+        /// Deletes the user.
+        /// <para>
+        /// This method deletes the user from the chat.
+        /// It will remove the user from all the channels and delete the user's data.
+        /// </para>
+        /// </summary>
+        /// <exception cref="PubNubCCoreException">
+        /// This exception might be thrown when any error occurs while deleting the user.
+        /// </exception>
+        /// <example>
+        /// <code>
+        /// var user = // ...;
+        /// user.DeleteUser();
+        /// </code>
+        /// </example>
         public void DeleteUser()
         {
             chat.DeleteUser(Id);
         }
 
+        /// <summary>
+        /// Sets restrictions on the user.
+        /// <para>
+        /// This method sets the restrictions on the user.
+        /// You can ban the user from a channel, mute the user on the channel, or set the restrictions on the user.
+        /// </para>
+        /// </summary>
+        /// <param name="channelId">The channel id on which the restrictions are set.</param>
+        /// <param name="banUser">If set to <c>true</c>, the user is banned from the channel.</param>
+        /// <param name="muteUser">If set to <c>true</c>, the user is muted on the channel.</param>
+        /// <param name="reason">The reason for setting the restrictions on the user.</param>
+        /// <exception cref="PubNubCCoreException">
+        /// This exception might be thrown when any error occurs while setting the restrictions on the user.
+        /// </exception>
+        /// <example>
+        /// <code>
+        /// var user = // ...;
+        /// user.SetRestrictions("channel_id", true, false, "Banned from the channel");
+        /// </code>
+        /// </example>
         public void SetRestrictions(string channelId, bool banUser, bool muteUser, string reason)
         {
             chat.SetRestrictions(Id, channelId, banUser, muteUser, reason);
         }
 
-        public string GetChannelRestrictions(string channelId, int limit, string startTimeToken, string endTimeToken)
+       public string GetChannelRestrictions(string channelId, int limit, string startTimeToken, string endTimeToken)
         {
             var buffer = new StringBuilder(8192);
             CUtilities.CheckCFunctionResult(pn_user_get_channel_restrictions(pointer, Id, channelId, limit,
