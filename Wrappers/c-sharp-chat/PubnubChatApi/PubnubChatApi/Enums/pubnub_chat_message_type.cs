56e992ea544034354b4d977e564534bd460fa510
Xavrax <matedahlke@gmail.com>
2024-06-17 15:20:14 +0200

    enum

--- Wrappers/c-sharp-chat/PubnubChatApi/PubnubChatApi/Enums/pubnub_chat_message_type.cs
+++ Wrappers/c-sharp-chat/PubnubChatApi/PubnubChatApi/Enums/pubnub_chat_message_type.cs
@@ -1,5 +1,11 @@
 namespace PubnubChatApi.Enums
 {
+    /// <summary>
+    /// Represents the type of a chat message.
+    /// <para>
+    /// Chat messages can have different types, such as text messages or message actions.
+    /// </para>
+    /// </summary>
     public enum pubnub_chat_message_type
     {
         PCMT_TEXT
