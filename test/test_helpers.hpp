ebce8488977e55b407b4e625c6cf77e96e65797f
Xavrax <matedahlke@gmail.com>
2024-10-18 13:11:22 +0200

    presence unit tests

--- test/test_helpers.hpp
+++ test/test_helpers.hpp
@@ -11,4 +11,13 @@ Pubnub::String trim_whitespaces(Pubnub::String to_trim) {
     return to_trim;
 }
 
+template <typename T>
+bool vector_contains(const std::vector<T>& vec, const T& value) {
+    return std::find_if(
+            vec.begin(),
+            vec.end(),
+            [&value](auto other) {return value == other;}
+        ) != vec.end();
+}
+
 
