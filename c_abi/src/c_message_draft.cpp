419071ff769a8d0218fb84ba5ede255d87a0055b
Xavrax <matedahlke@gmail.com>
2024-11-08 10:09:18 +0100

    deserialize target

--- c_abi/src/c_message_draft.cpp
+++ c_abi/src/c_message_draft.cpp
@@ -1,5 +1,6 @@
 #include "c_message_draft.hpp"
 #include "c_errors.hpp"
+#include "domain/json.hpp"
 #include "message_draft.hpp"
 #include <exception>
 
@@ -23,9 +24,19 @@ void pn_message_draft_remove_text(Pubnub::MessageDraft* message_draft, size_t po
     }
 }
 
-void pn_message_draft_add_mention(Pubnub::MessageDraft* message_draft, size_t start, size_t length, const Pubnub::MentionTarget* target) {
+void pn_message_draft_add_mention(Pubnub::MessageDraft* message_draft, size_t start, size_t length, const char* target) {
     try {
-        message_draft->add_mention(start, length, *target);
+        auto parsed_target = Json::parse(target);
+        auto target_type = parsed_target.get_int("type").value();
+        auto target_value = parsed_target.get_string("target").value();
+
+        auto mention_target = target_type == PN_MESSAGE_DRAFT_MENTION_TARGET_TYPE_USER 
+            ? Pubnub::MentionTarget::user(target_value) 
+            : target_type == PN_MESSAGE_DRAFT_MENTION_TARGET_TYPE_CHANNEL 
+                ? Pubnub::MentionTarget::channel(target_value) 
+                : Pubnub::MentionTarget::url(target_value);
+
+        message_draft->add_mention(start, length, mention_target);
     } catch (const std::exception& e) {
         pn_c_set_error_message(e.what());
     }
