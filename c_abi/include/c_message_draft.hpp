3a4d9464625a46270a3e65d3ef94c195fe27da44
Xavrax <matedahlke@gmail.com>
2024-11-08 09:19:20 +0100

    message draft without callbacks for cabi

--- c_abi/include/c_message_draft.hpp
+++ c_abi/include/c_message_draft.hpp
@@ -8,4 +8,16 @@
 
 PN_CHAT_EXTERN PN_CHAT_EXPORT void pn_message_draft_delete(Pubnub::MessageDraft* message_draft);
 
+PN_CHAT_EXTERN PN_CHAT_EXPORT void pn_message_draft_insert_text(Pubnub::MessageDraft* message_draft, std::size_t position, const char* text_to_insert);
+
+PN_CHAT_EXTERN PN_CHAT_EXPORT void pn_message_draft_remove_text(Pubnub::MessageDraft* message_draft, std::size_t position, std::size_t length);
+
+PN_CHAT_EXTERN PN_CHAT_EXPORT void pn_message_draft_add_mention(Pubnub::MessageDraft* message_draft, std::size_t start, std::size_t length, const char* mention_json);
+
+PN_CHAT_EXTERN PN_CHAT_EXPORT void pn_message_draft_remove_mention(Pubnub::MessageDraft* message_draft, std::size_t start);
+
+PN_CHAT_EXTERN PN_CHAT_EXPORT void pn_message_draft_update(Pubnub::MessageDraft* message_draft, const char* text);
+
+PN_CHAT_EXTERN PN_CHAT_EXPORT void pn_message_draft_send(Pubnub::MessageDraft* message_draft, const Pubnub::SendTextParams* send_params);
+
 #endif // PN_CHAT_C_MESSAGE_DRAFT_H
--- c_abi/src/c_message_draft.cpp
+++ c_abi/src/c_message_draft.cpp
@@ -1,6 +1,58 @@
 #include "c_message_draft.hpp"
 #include "c_errors.hpp"
+#include "message_draft.hpp"
+#include <exception>
 
 void pn_message_draft_delete(Pubnub::MessageDraft* message_draft) {
     delete message_draft;
 }
+
+void pn_message_draft_insert_text(Pubnub::MessageDraft* message_draft, size_t position, const char* text) {
+    try {
+        message_draft->insert_text(position, text);
+    } catch (const std::exception& e) {
+        pn_c_set_error_message(e.what());
+    }
+}
+
+void pn_message_draft_remove_text(Pubnub::MessageDraft* message_draft, size_t position, size_t length) {
+    try {
+        message_draft->remove_text(position, length);
+    } catch (const std::exception& e) {
+        pn_c_set_error_message(e.what());
+    }
+}
+
+void pn_message_draft_add_mention(Pubnub::MessageDraft* message_draft, size_t start, size_t length, const Pubnub::MentionTarget* target) {
+    try {
+        message_draft->add_mention(start, length, *target);
+    } catch (const std::exception& e) {
+        pn_c_set_error_message(e.what());
+    }
+}
+
+void pn_message_draft_remove_mention(Pubnub::MessageDraft* message_draft, size_t start) {
+    try {
+        message_draft->remove_mention(start);
+    } catch (const std::exception& e) {
+        pn_c_set_error_message(e.what());
+    }
+}
+
+void pn_message_draft_update(Pubnub::MessageDraft* message_draft, const char* text) {
+    try {
+        message_draft->update(text);
+    } catch (const std::exception& e) {
+        pn_c_set_error_message(e.what());
+    }
+}
+
+void pn_message_draft_send(Pubnub::MessageDraft* message_draft, const Pubnub::SendTextParams* send_params) {
+    try {
+        message_draft->send(*send_params);
+    } catch (const std::exception& e) {
+        pn_c_set_error_message(e.what());
+    }
+}
+
+
