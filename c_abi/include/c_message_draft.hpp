34aca73c78b1402fdc3be32866bfcc847a5eb96b
PUBNUB\jakub.grzesiowski <jakub.grzesiowski@pubnub.com>
2024-11-14 13:24:08 +0100

    fix c message draft header

--- c_abi/include/c_message_draft.hpp
+++ c_abi/include/c_message_draft.hpp
@@ -12,13 +12,13 @@ PN_CHAT_EXTERN PN_CHAT_EXPORT void pn_message_draft_insert_text(Pubnub::MessageD
 
 PN_CHAT_EXTERN PN_CHAT_EXPORT void pn_message_draft_remove_text(Pubnub::MessageDraft* message_draft, std::size_t position, std::size_t length);
 
-PN_CHAT_EXTERN PN_CHAT_EXPORT void pn_message_draft_insert_suggested_mention(Pubnub::MessageDraft* message_draft, std::size_t offset, const char* replace_from, const char* replace_to, const char* target_json);
+PN_CHAT_EXTERN PN_CHAT_EXPORT void pn_message_draft_insert_suggested_mention(Pubnub::MessageDraft* message_draft, std::size_t offset, const char* replace_from, const char* replace_to, const char* target_json, const char* text);
 
 #define PN_MESSAGE_DRAFT_MENTION_TARGET_TYPE_USER 0
 #define PN_MESSAGE_DRAFT_MENTION_TARGET_TYPE_CHANNEL 1
 #define PN_MESSAGE_DRAFT_MENTION_TARGET_TYPE_URL 2
 
-PN_CHAT_EXTERN PN_CHAT_EXPORT void pn_message_draft_add_mention(Pubnub::MessageDraft* message_draft, std::size_t start, std::size_t length, const char* mention_json, const char* text);
+PN_CHAT_EXTERN PN_CHAT_EXPORT void pn_message_draft_add_mention(Pubnub::MessageDraft* message_draft, std::size_t start, std::size_t length, const char* target);
 
 PN_CHAT_EXTERN PN_CHAT_EXPORT void pn_message_draft_remove_mention(Pubnub::MessageDraft* message_draft, std::size_t start);
 
