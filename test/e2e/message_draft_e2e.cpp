ec938dc580f22156c405cbdb321a52bc95c1969d
PUBNUB\jakub.grzesiowski <jakub.grzesiowski@pubnub.com>
2025-02-26 13:46:08 +0100

    WIP on implementing Google Test

--- test/e2e/message_draft_e2e.cpp
+++ test/e2e/message_draft_e2e.cpp
@@ -1,37 +1,36 @@
 #include "chat.hpp"
 #include "channel.hpp"
+#include "message_draft.hpp"
 #include "pubnub_chat/chat.hpp"
-#include "cgreen/cgreen.h"
-#include "cgreen/unit.h"
+#include <gtest/gtest.h>
 
-using namespace cgreen;
+class MessageDraftTests : public :: testing::Test {
+    protected:
+        std::unique_ptr<Pubnub::Chat> chat;
+        std::unique_ptr<Pubnub::Channel> channel;
 
-Describe(MessageDrafts);
-std::unique_ptr<Pubnub::Chat> chat;
-std::unique_ptr<Pubnub::Channel> channel;
+        // This method will be called before each test in the test suite  
+        void SetUp() override {
+            Pubnub::String publish_key = std::getenv("PUBNUB_PUBLISH_KEY");
+            Pubnub::String subscribe_key = std::getenv("PUBNUB_SUBSCRIBE_KEY");
 
-BeforeEach(MessageDrafts) {
-    Pubnub::String publish_key = std::getenv("PUBNUB_PUBLISH_KEY");
-    Pubnub::String subscribe_key = std::getenv("PUBNUB_SUBSCRIBE_KEY");
+            if (publish_key.empty() || subscribe_key.empty()) {
+                throw std::runtime_error("PUBNUB_PUBLISH_KEY and PUBNUB_SUBSCRIBE_KEY must be set");
+            }
 
-    if (publish_key.empty() || subscribe_key.empty()) {
-        throw std::runtime_error("PUBNUB_PUBLISH_KEY and PUBNUB_SUBSCRIBE_KEY must be set");
+            chat.reset(new Pubnub::Chat(Pubnub::Chat::init(
+                publish_key, subscribe_key, "test_id", Pubnub::ChatConfig())));
+
+            channel.reset(new Pubnub::Channel(chat->create_public_conversation("test_channel", Pubnub::ChatChannelData{})));
+        }
+
+        // This method will be called after each test in the test suite  
+        void TearDown() override {
+            // Do your cleanup operations here  
+        }
+
+    TEST_F(MessageDraft_GoogleTests, TestCreateMessageDraft) {
+        ASSERT_NO_THROW(channel->create_message_draft());
     }
-
-    chat.reset(new Pubnub::Chat(Pubnub::Chat::init(
-                    publish_key, subscribe_key, "test_id", Pubnub::ChatConfig())));
-
-    channel.reset(new Pubnub::Channel(chat->create_public_conversation("test_channel", Pubnub::ChatChannelData{})));
-}
-
-AfterEach(MessageDrafts) { /* No cleanup needed */ }
-
-Ensure(MessageDrafts, should_prepare_message_to_send) {
-    sut = channel->create_message_draft();
-
-    sut->insert_text(0, "typoHello, world!");
-    sut->remove_text(0, 4);
-
-    sut->send();
-}
+};
 
