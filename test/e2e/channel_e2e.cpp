675222b3ebca1817c97dc369b1615e0f80d35070
PUBNUB\jakub.grzesiowski <jakub.grzesiowski@pubnub.com>
2025-03-10 15:34:19 +0100

    Add more tests - channel and message draft

---
+++ test/e2e/channel_e2e.cpp
@@ -0,0 +1,217 @@
+#include "chat.hpp"
+#include "channel.hpp"
+#include "pubnub_chat/chat.hpp"
+#include <gtest/gtest.h>
+#include "string.hpp"
+#include "pubnub_chat/message_draft.hpp"
+#include "pubnub_chat/message.hpp"
+#include "pubnub_chat/chat.hpp"
+#include "pubnub_chat/vector.hpp"
+#include "pubnub_chat/enums.hpp"
+#include <algorithm>
+#include <thread>
+#include <vector>  
+#include <string>  
+#include <iostream>  
+
+class ChannelTests : public ::testing::Test {
+protected:
+    std::unique_ptr<Pubnub::Chat> chat;
+
+    // This method will be called before each test in the test suite  
+    void SetUp() override {
+        Pubnub::String publish_key = std::getenv("PUBNUB_PUBLISH_KEY");
+        if (publish_key.empty()) {
+            publish_key = "demo-36";
+        }
+        Pubnub::String subscribe_key = std::getenv("PUBNUB_SUBSCRIBE_KEY");
+        if (subscribe_key.empty()) {
+            subscribe_key = "demo-36";
+        }
+
+        chat.reset(new Pubnub::Chat(Pubnub::Chat::init(
+            publish_key, subscribe_key, "channel_tests_user", Pubnub::ChatConfig())));
+    }
+
+    // This method will be called after each test in the test suite  
+    void TearDown() override {
+        // Do your cleanup operations here  
+    }
+};
+
+TEST_F(ChannelTests, TestGetUserSuggestions) {
+    auto current_user = chat->current_user();
+    auto user_data = Pubnub::ChatUserData{};
+    user_data.user_name = "Testinski";
+    current_user.update(user_data);
+
+    std::this_thread::sleep_for(std::chrono::seconds(4));
+
+    auto channel = chat->create_public_conversation("user_suggestions_channel", Pubnub::ChatChannelData{});
+    channel.join([&channel](Pubnub::Message message) {});
+
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+
+    auto suggestions = chat->get_user_suggestions("@Test");
+
+    auto contains_name = std::any_of(suggestions.begin(), suggestions.end(), [](const Pubnub::User& user) {
+        return user.user_data().user_name == Pubnub::String("Testinski");
+        });
+
+    ASSERT_TRUE(contains_name);
+}
+
+TEST_F(ChannelTests, TestStartTyping) {
+    auto current_user = chat->current_user();
+    Pubnub::User talk_user;
+    try
+    {
+        talk_user = chat->get_user("typing_test_user");
+    }
+    catch (const std::exception&)
+    {
+        talk_user = chat->create_user("typing_test_user", Pubnub::ChatUserData{});
+    }
+    auto channel = chat->create_direct_conversation(talk_user, "typing_test_channel", Pubnub::ChatChannelData{}).created_channel;
+    channel.join([&channel](Pubnub::Message message) {});
+
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+
+    auto got_typing_callback = false;
+    channel.get_typing([&](Pubnub::Vector<Pubnub::String> typing_users) {
+        for (size_t i = 0; i < typing_users.size(); i++)
+        {
+            if (typing_users[i] == Pubnub::String("channel_tests_user")) {
+                got_typing_callback = true;
+                break;
+            }
+        }
+        });
+    channel.start_typing();
+
+    std::this_thread::sleep_for(std::chrono::seconds(4));
+
+    ASSERT_TRUE(got_typing_callback);
+}
+
+TEST_F(ChannelTests, TestStopTyping) {
+    auto current_user = chat->current_user();
+    Pubnub::User talk_user;
+    try
+    {
+        talk_user = chat->get_user("typing_stop_test_user");
+    }
+    catch (const std::exception&)
+    {
+        talk_user = chat->create_user("typing_stop_test_user", Pubnub::ChatUserData{});
+    }
+    auto channel = chat->create_direct_conversation(talk_user, "typing_test_channel", Pubnub::ChatChannelData{}).created_channel;
+    channel.join([&channel](Pubnub::Message message) {});
+
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+
+    channel.start_typing();
+
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+
+    auto got_stopped_typing_callback = false;
+    channel.get_typing([&](Pubnub::Vector<Pubnub::String> typing_users) {
+        if (typing_users.size() == 0) {
+            got_stopped_typing_callback = true;
+        }
+        });
+    channel.stop_typing();
+    
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+
+    ASSERT_TRUE(got_stopped_typing_callback);
+}
+
+TEST_F(ChannelTests, TestStopTypingFromTimer) {
+    auto current_user = chat->current_user();
+    Pubnub::User talk_user;
+    try
+    {
+        talk_user = chat->get_user("typing_stop_test_user");
+    }
+    catch (const std::exception&)
+    {
+        talk_user = chat->create_user("typing_stop_test_user", Pubnub::ChatUserData{});
+    }
+    auto channel = chat->create_direct_conversation(talk_user, "typing_test_channel", Pubnub::ChatChannelData{}).created_channel;
+    channel.join([&channel](Pubnub::Message message) {});
+
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+
+    channel.start_typing();
+
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+
+    auto got_stopped_typing_callback = false;
+    channel.get_typing([&](Pubnub::Vector<Pubnub::String> typing_users) {
+        std::cout << "AAAAAAAAAAAAAA" << std::endl;
+        for (size_t i = 0; i < typing_users.size(); i++)
+        {
+            std::cout << typing_users[i] << std::endl;
+        }
+        if (typing_users.size() == 0) {
+            got_stopped_typing_callback = true;
+        }
+        });
+
+    std::this_thread::sleep_for(std::chrono::seconds(10));
+
+    ASSERT_TRUE(got_stopped_typing_callback);
+}
+
+TEST_F(ChannelTests, TestPinAndUnPinMessage) {
+    auto channel = chat->create_public_conversation("pin_message_test_channel", Pubnub::ChatChannelData{});
+    auto pinned = false;
+    auto unpinned = false;
+    channel.join([&](Pubnub::Message message) {
+        channel = channel.pin_message(message);
+        try
+        {
+            auto pinned_message = channel.get_pinned_message();
+            if (pinned_message.text() == Pubnub::String("message_to_pin")) {
+                pinned = true;
+            }
+        }
+        catch (const std::exception&)
+        {
+            pinned = false;
+        }
+        channel = channel.unpin_message();
+        try
+        {
+            auto pinned_message = channel.get_pinned_message();
+            unpinned = false;
+        }
+        catch (const std::exception&)
+        {
+            unpinned = true;
+        }
+        });
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+    channel.send_text("message_to_pin");
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+    ASSERT_TRUE(pinned);
+    ASSERT_TRUE(unpinned);
+}
+
+TEST_F(ChannelTests, TestEmitUserMention) {
+    auto channel = chat->create_public_conversation("user_mention_test_channel", Pubnub::ChatChannelData{});
+    channel.join([&](Pubnub::Message message) {});
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+    auto mentioned = false;
+    chat->listen_for_events("channel_tests_user", Pubnub::PCET_MENTION, [&](Pubnub::Event event_object) {
+        std::cout << "AAAAAA " << event_object.payload << std::endl;
+        if (event_object.payload.find("heyyyy") != std::string::npos) {
+            mentioned = true;
+        }
+        });
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+    channel.emit_user_mention("channel_tests_user", "99999999999999999", "heyyyy");
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+    ASSERT_TRUE(mentioned);
+}
--- test/e2e/message_draft_e2e.cpp
+++ test/e2e/message_draft_e2e.cpp
@@ -1,10 +1,14 @@
 #include "chat.hpp"
 #include "channel.hpp"
-#include "message_draft.hpp"
 #include "pubnub_chat/chat.hpp"
 #include <gtest/gtest.h>
 #include "string.hpp"
-#include "helpers/export.hpp"
+#include "pubnub_chat/message_draft.hpp"
+#include "pubnub_chat/message.hpp"
+#include "pubnub_chat/chat.hpp"
+#include "pubnub_chat/vector.hpp"
+#include <algorithm>
+#include <thread>
 
 class MessageDraftTests : public :: testing::Test {
     protected:
@@ -14,10 +18,12 @@ class MessageDraftTests : public :: testing::Test {
         // This method will be called before each test in the test suite  
         void SetUp() override {
             Pubnub::String publish_key = std::getenv("PUBNUB_PUBLISH_KEY");
+            if (publish_key.empty()) {
+                publish_key = "pub-c-79c582a2-d7a4-4ee7-9f28-7a6f1b7fa11c";
+            }
             Pubnub::String subscribe_key = std::getenv("PUBNUB_SUBSCRIBE_KEY");
-
-            if (publish_key.empty() || subscribe_key.empty()) {
-                throw std::runtime_error("PUBNUB_PUBLISH_KEY and PUBNUB_SUBSCRIBE_KEY must be set");
+            if (subscribe_key.empty()) {
+                subscribe_key = "sub-c-ca0af928-f4f9-474c-b56e-d6be81bf8ed0";
             }
 
             chat.reset(new Pubnub::Chat(Pubnub::Chat::init(
@@ -36,3 +42,31 @@ TEST_F(MessageDraftTests, TestCreateMessageDraft) {
     ASSERT_NO_THROW(channel->create_message_draft());
 }
 
+TEST_F(MessageDraftTests, TestInsertAndRemoveText) {
+    auto message_draft = channel->create_message_draft();
+
+    auto insert_success = false;
+    auto remove_success = false;
+    message_draft.add_change_listener([&](const Pubnub::Vector<Pubnub::MessageElement>& elements) {
+        std::for_each(elements.begin(), elements.end(), [&](const Pubnub::MessageElement& element) {
+            if (element.text == Pubnub::String("test insert")) {
+                insert_success = true;
+            }
+            else if (element.text == Pubnub::String("insert")) {
+                remove_success = true;
+            }
+            });
+        }
+    );
+    message_draft.insert_text(0, "test insert");
+    
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+
+    ASSERT_TRUE(insert_success);
+
+    message_draft.remove_text(0, 5);
+
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+
+    ASSERT_TRUE(remove_success);
+}
